875a4b65257ad563445b67f5a6f87b1e08d11f57
ks-mint <ksuniqum.gl@gmail.com>
2025-11-28 15:01:43 +0100

    add LoginPage.jsx bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
 import {Link, Route, Routes} from "react-router-dom";
 import NotFoundPage from "./components/NotFoundPage.jsx";
+import LoginPage from "./components/LoginPage.jsx";
 
 function HomePage() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <h1>Тут будет чат</h1>
             <Link to="/login">Войти в чат</Link>
         </>
     )
 }
 
-function LoginPage() {
-    return (
-        <>
-            <Link to="/">Main page</Link>
-            <h1>Страница входа</h1>
-        </>
-    )
-}
+
 
 function App() {
     return (
