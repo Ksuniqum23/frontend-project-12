257837f50cc862ec3ac1a8283a889ce73849819e
ks-mint <ksuniqum.gl@gmail.com>
2025-12-08 15:28:34 +0100

    chatHeader counter works

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,8 +1,18 @@
-export default function ChatHeader({ activeChannel, messageCount = 0 }) {
+import {useSelector} from "react-redux";
+import {selectChannelById} from "../store/channelsSlice.js";
+import {selectMessagesByChannel} from "../store/messagesSlice.js";
+
+export default function ChatHeader() {
+    const activeChannelId = useSelector((state) => state.channels.activeChannelId);
+    const activeChannel = useSelector((state) =>
+        selectChannelById(state, activeChannelId)
+    );
+    const messages = useSelector(selectMessagesByChannel(activeChannelId));
+
     return (
         <div className="bg-light mb-4 p-3 shadow-sm small">
             <p className="m-0"><b>{activeChannel ? `# ${activeChannel.name}` : '# —'}</b></p>
-            <span className="text-muted">{messageCount} сообщений</span>
+            <span className="text-muted">{messages.length} сообщений</span>
         </div>
     )
 }
