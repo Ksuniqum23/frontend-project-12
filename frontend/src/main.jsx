ae597884a99a58c8a11f4b20df49fa433c68b289
ks-mint <ksuniqum.gl@gmail.com>
2025-12-15 13:18:39 +0100

    eslint v3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,12 +9,11 @@ import './index.css'
 import { store } from './store'
 import SocketProvider from './socket/SocketProvider.jsx'
 import App from './App.jsx'
-import {rollbarConfig} from "./rollbar/rollbarConfig.jsx"
-import Rollbar from "rollbar";
+import { rollbarConfig } from "./rollbar/rollbarConfig.jsx"
+import Rollbar from "rollbar"
 
 const rollbar = new Rollbar(rollbarConfig)
 window.rollbar = rollbar
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -25,5 +24,5 @@ createRoot(document.getElementById('root')).render(
         </SocketProvider>
       </BrowserRouter>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
--- frontend/src/rollbar/rollbarConfig.jsx
+++ frontend/src/rollbar/rollbarConfig.jsx
@@ -1,6 +1,3 @@
-import React from 'react'
-import { Provider, ErrorBoundary } from '@rollbar/react'
-
 export const rollbarConfig = {
   accessToken: '774c64581eb44a8e9a2c89ce9cc3d9ad',
   environment: 'testenv',
